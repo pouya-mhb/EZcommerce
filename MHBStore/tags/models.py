a800e201cbee860acb4b1cd8b66206b27cbc4d07
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-02-15 09:11:10 +0330

    add generic relationship

--- MHBStore/tags/models.py
+++ MHBStore/tags/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# ----- Generic Relationships -----
+'''
+to define a generic relationship we need to define 3 fields
+content_type
+object_id
+content_object
+'''
+
+
+class Tag (models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem (models.Model):
+    # what tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # type (product, video, article)
+    # id
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    # read the actual object that particiular tag applied to
+    content_object = GenericForeignKey()
+
+
+'''
+LikedItem
+    what user likes what object
+    user : ForienKey to User
+'''
+
+
+class LikedItem (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
