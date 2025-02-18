54f8aac93eca0d39127e42595bcf1ae3dd27892f
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-02-18 19:17:47 +0330

    add LikedItem model

--- MHBStore/likes/models.py
+++ MHBStore/likes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
