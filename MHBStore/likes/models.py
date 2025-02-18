44fac51752332d875054f36a0124cd5d5ccc884c
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2025-02-18 19:24:35 +0330

    bugfix

--- MHBStore/likes/models.py
+++ MHBStore/likes/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class LikedItem(models.Model):
